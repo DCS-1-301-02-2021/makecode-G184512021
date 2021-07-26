5fb61664a64f266500dd25eaa58291d4f643d4e8
G184512021 <86589890+G184512021@users.noreply.github.com>
2021-07-26 16:22:19 +0900

    ゲームを完成させた

---
+++ images.g.ts
@@ -0,0 +1,37 @@
+// 自動生成されたコードです。変更しないでください。
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "bogy":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . 4 4 4 4 4 4 4 4 . . . . . . 
+. 4 4 4 4 4 4 4 4 4 4 . . . . . 
+4 4 1 1 4 4 4 4 4 4 4 4 4 4 4 4 
+4 4 1 1 4 4 4 4 4 4 2 2 2 2 2 2 
+4 4 4 4 4 4 4 4 4 4 2 2 2 2 2 2 
+4 4 4 4 4 4 4 4 4 4 4 4 4 3 4 4 
+4 4 4 4 4 4 4 4 4 2 2 2 2 2 2 . 
+4 4 4 4 4 4 4 4 4 2 2 2 2 2 . . 
+4 4 4 4 4 4 4 4 4 4 4 4 4 . . . 
+. 4 4 4 4 4 4 4 4 4 4 4 . . . . 
+. . 2 2 2 2 2 2 2 2 . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// 自動生成されたコードです。変更しないでください。
